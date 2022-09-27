b070bd47fe19b284114a0230773dbd21a3c2043d
PacersCyc <452720855@qq.com>
2022-09-28 00:18:11 +0800

    feat: login & register

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'', include(('learning_logs.urls', 'learning_logs'), namespace = "learning_logs"))
+    re_path(r'^users/', include(('users.urls', 'users'), namespace="users")),
+    re_path(r'', include(('learning_logs.urls', 'learning_logs'), namespace = "learning_logs")),
 ]
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
   # 显示所有主题
   re_path(r'^topics/$', views.topics, name = "topics"),
   # 特定主题的详细页面
-  re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = "topic")
+  re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = "topic"),
+  # 添加主题页
+  re_path(r'^new_topic/$', views.new_topic, name = "new_topic"),
+  # 添加条目页
+  re_path(r'^new_entry/(?P<topic_id>\d+)$', views.new_entry, name = "new_entry"),
+  # 编辑条目页
+  re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name = "edit_entry"),
 ]
